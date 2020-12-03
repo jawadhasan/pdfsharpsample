663527878a3ba07667bd2296d66833639e13dff1
Jawad Hasan <jawadhasan80@gmail.com>
2020-12-03 01:36:54 +0100

    console app pdf demo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 
 namespace pdfsharpsample
 {
@@ -6,7 +9,35 @@ namespace pdfsharpsample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //https://stackoverflow.com/questions/49215791/vs-code-c-sharp-system-notsupportedexception-no-data-is-available-for-encodin
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            Console.WriteLine("Generating PDF...");
+            
+            // Create a new PDF document
+            var document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+
+            // Create an empty page
+            var page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            var gfx = XGraphics.FromPdfPage(page);
+
+            // Create a font
+            var font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+
+            // Draw the text
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+            new XRect(0, 0, page.Width, page.Height),
+            XStringFormats.Center);
+
+            // Save the document...
+            var filename = "HelloWorld.pdf";
+            document.Save(filename);
+            Console.WriteLine("PDF Generated!");
+
+            // ...and start a viewer.
+            //Process.Start(filename);
         }
     }
 }
